155994bd921f8ef83bfc11b23bbe17e414f94479
RustyQuasar <dvwninja@outlook.com>
2025-09-18 15:13:08 -0400

    Commended config info

--- TeamCode/src/main/java/Utilities/Constants.java
+++ TeamCode/src/main/java/Utilities/Constants.java
@@ -7,12 +7,12 @@ public class Constants {
     public static final int defaultDCRPS = (100/60); //https://docs.wsr.studica.com/en/latest/docs/Motors/maverick-dc-motor.html
     public static final class DriveTrainConstants {
 
-        //Wheel constants
-        public static final String frontLeftMotor0 = "frontLeft";
-        public static final String frontRightMotor1 = "frontRight";
-        public static final String backLeftMotor2 = "backLeft";
-        public static final String backRightMotor3 = "backRight";
-        //Gyro
+        //Wheel constants (Studica Mavericks)
+        public static final String frontLeftMotor0 = "frontLeft"; //Control hub Motor port 0
+        public static final String frontRightMotor1 = "frontRight"; //Control hub Motor port 1
+        public static final String backLeftMotor2 = "backLeft"; //Control hub Motor port 2
+        public static final String backRightMotor3 = "backRight"; //Control hub Motor port 3
+        //Gyro (12ICU 0) Something like that idk
         public static final String imu = "imu";
         //idk
         public static final double controlHubOffset = 0;
@@ -21,18 +21,23 @@ public class Constants {
     }
 
     public static final class IntakeConstants {
+        //Servo0 (Continuous Rotation - Control Hub)
         public static final String intake = "intake";
+        //Digital 0:1 (Colour Sensor - Control Hub)
         public static final String colourSensor = "colourSensor"; //#TODO: Figure out what it's actually called and configure accordingly
     }
 
     public static final class ShooterConstants {
-        public static final String leftShooter0 = "leftShooter";
-        public static final String rightShooter1 = "rightShooter";
+        //Studica Mavericks
+        public static final String leftShooter0 = "leftShooter"; //Expansion hub Motor port 0
+        public static final String rightShooter1 = "rightShooter"; //Expansion hub Motor port 0
         public static final double shooterGearRatio = (61 * 1); //Gear multiplier
         //TODO: Get actual hardware for neck, head and transfer in configuration
-        public static final String turretNeck = "turretNeck";
-        public static final String turretHead = "turretHead";
-        public static final String transferServo = "transferServo";
+        //Servos
+        public static final String turretNeck = "turretNeck"; //Expansion hub Servo port 0
+        public static final String turretHead = "turretHead"; //Expansion hub Servo port 1
+        //CR Servo
+        public static final String transferServo = "transferServo"; //Expansion hub Servo port 2
         public static final double turretNeckGearRatio = 2; //Gear multiplier
         public static final double turretHeadGearRatio = 2; //Gear multiplier
         public static final double turretTransferGearRatio = 2; //Gear multiplier
@@ -42,7 +47,8 @@ public class Constants {
     }
 
     public static final class VisionConstants {
-        public static final String camera = "Webcam";
+        //Webcam
+        public static final String camera = "Webcam"; //USB port
         public static final int resX = 640;
         public static final int resY = 480;
         public static final int FOV = 55;
